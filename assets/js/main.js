db478672dae3dd138cd9b5dc7a5c8c065917c223
Carme <carme.leticia@gmail.com>
2023-10-18 23:03:45 -0300

    diminuindo a verbosidade do código

--- assets/js/main.js
+++ assets/js/main.js
@@ -20,14 +20,22 @@ function convertPokemonToLi(pokemon){
 
 const pokemonList = document.getElementById('pokemonList')
 
-pokeApi.getPokemons().then((pokemons) =>{
-    const listItems = []
+pokeApi.getPokemons().then((pokemons = []) => {
 
-    for (let i = 0; i < pokemons.length; i++) {
-        const pokemon = pokemons[i];
-        listItems.push(convertPokemonToLi(pokemon)) 
-    }
+    const newList = pokemons.map((pokemon) => {
+        return convertPokemonToLi(pokemon)
+    })
 
-    console.log(listItems)
-})
+    const newHtml = newList.join('')
 
+    pokemonList.innerHTML += newHtml
+
+//const listItems = []
+
+//for (let i = 0; i < pokemons.length; i++) {
+ ///const pokemon = pokemons[i];
+//listItems.push(convertPokemonToLi(pokemon)) 
+  //  }
+
+    //console.log(listItems)
+})  
